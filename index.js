0955bc732280ebc1e5aba32d4fa1d4518f3791f7
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-02 13:44:22 +0600

    JWT is added with all reuirement

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app =express();
 const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 //middleware
@@ -31,6 +32,31 @@ async function run() {
    const userCollection = client.db("localeFoodDB").collection("users")
    const productCollection = client.db("localeFoodDB").collection("products")
 
+
+
+  //jwt related api
+  app.post('/jwt',async(req,res)=>{
+    const user =req.body
+    const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'24h'})
+    res.send({token})
+  })
+
+  //verify token milldlewares
+
+  const verifyToken =(req,res,next)=>{
+    console.log('inside verfiy token',req.headers);
+    if(!req.headers.authorization){
+      return res.status(401).send({message: 'forbidden access'})
+    }
+    const token = req.headers.authorization.split(' ')[1]
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({message: 'forbidden access'})
+      }
+      req.decoded =decoded
+      next()
+    })
+  }
 
    //users related api
    app.post('/users',async(req,res)=>{
@@ -62,14 +88,14 @@ app.get('/users/customer',async(req,res)=>{
 })
 
 //admin list
-app.get('/users/admin',async(req,res)=>{
+app.get('/users/admin',verifyToken,async(req,res)=>{
   const roll=req.query.roll
   const query = {roll: roll}
   const result =await userCollection.find(query).toArray()
   res.send(result)
 })
 //delete admin
-app.delete('/users/admin/:email',async(req,res)=>{
+app.delete('/users/admin/:email', async(req,res)=>{
   const email = req.params.email
   const query= {email: email}
   const result = await userCollection.deleteOne(query)
@@ -99,11 +125,11 @@ res.send(result)
 })
 
 //check admin
-app.get('/users/admin/:email',async(req,res)=>{
+app.get('/users/admin/:email',verifyToken,async(req,res)=>{
   const email =req.params.email
-  // if(email!==req.decoded.email){
-  //   return res.status(403).send({message:'unauthorized access'})
-  // }
+  if(email!==req.decoded.email){
+    return res.status(403).send({message:'unauthorized access'})
+  }
   const query={email:email}
   const user =await userCollection.findOne(query)
   let admin=false
@@ -116,6 +142,7 @@ app.get('/users/admin/:email',async(req,res)=>{
 
 //check vendor
 app.get('/users/vendor/:email',async(req,res)=>{
+  // console.log(req.headers);
   const email =req.params.email
   // if(email!==req.decoded.email){
   //   return res.status(403).send({message:'unauthorized access'})
